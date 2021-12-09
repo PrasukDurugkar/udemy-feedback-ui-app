c40d73048f789510e96831a9b46ae308c91e2a4a
Prasuk Durugkar <prasuk121@gmail.com>
2021-12-09 22:47:38 +0530

    learned uses of state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
 function App() {
-    return (
+  return (
+    <>
+      <Header />
       <div className="container">
-          <h1>My App</h1>
+        <FeedbackItem />
       </div>
-    ); 
+    </>
+  );
 }
-export default App
+export default App;
